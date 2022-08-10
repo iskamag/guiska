6cbf4df59aafb3a03010487217ca5eee87439d6f
iskam <iskamagg@gmail.com>
2022-08-10 23:37:53 +0500

    refactors/fixes 2 (made by unmatched-paren)

--- packages/tport.scm
+++ packages/tport.scm
@@ -2,6 +2,7 @@
     #:use-module (ice-9 match)
     #:use-module (guix packages)
     #:use-module (guix download)
+    #:use-module (guix gexp)
     #:use-module (guix git-download)
     #:use-module (guix build-system cmake)
     #:use-module ((guix licenses) #:prefix license:)
@@ -18,89 +19,60 @@
   (package
     (name "zmusic")
     (version "1.1.3")
-    (source
-     (origin
-       (method git-fetch)
-       (uri (git-reference
-             (url "https://github.com/ZDoom/ZMusic")
-             (commit version)))
-       (file-name (git-file-name name version))
-       (sha256
-        (base32 "0vpr79gpdbhslg5qxyd1qxlv5akgli26skm1vb94yd8v69ymdcy2"))
+    (source (origin
+              (method git-fetch)
+              (uri (git-reference
+                    (url "https://github.com/ZDoom/ZMusic")
+                    (commit version)))
+              (file-name (git-file-name name version))
+              (sha256
+               (base32
+                "0vpr79gpdbhslg5qxyd1qxlv5akgli26skm1vb94yd8v69ymdcy2"))
        ;(patches (search-patches "zmusic-search-in-installed-share.patch"
                                 ;"zmusic-find-system-libgme.patch"))
        (modules '((guix build utils)))
        (snippet
-        '(begin
-           ;; Remove some bundled libraries.  XXX There are more, but removing
-           ;; them would require, at least, patching the build system.
-           (with-directory-excursion "thirdparty"
-             (delete-file-recursively "game-music-emu")
-             (delete-file-recursively "zlib"))
-           #t))))
-    (arguments
-     '(#:tests? #f
-       #:configure-flags
-       (let ((out (assoc-ref %outputs "out")))
-         (list
-          (string-append
-           "-DCMAKE_CXX_FLAGS:="
-           "-DSHARE_DIR=\\\"" out "/share/\\\" "
-           "-DGUIX_OUT_PK3=\\\"" out "/share/games/doom\\\"")
-
-          ;; The build requires some extra convincing not to use the bundled
-          ;; libgme previously deleted in the soure snippet.
-          "-DFORCE_INTERNAL_GME=OFF"
-
-          ;; Link libraries at build time instead of loading them at run time.
-          "-DDYN_OPENAL=OFF"
-          "-DDYN_FLUIDSYNTH=OFF"
-          "-DDYN_MPG123=OFF"
-          "-DDYN_SNDFILE=OFF"))
-
-       #:phases
-       (modify-phases %standard-phases
-         (add-before 'configure 'fix-referenced-paths
-           (lambda* (#:key inputs outputs #:allow-other-keys)
-             (let ((fluid-3 (assoc-ref inputs "fluid-3"))
-                   (timidity++ (assoc-ref inputs "timidity++"))
-                   (out (assoc-ref outputs "out"))
-
-                    ;(bin (string-append out "/bin"))
-                    ;(share (string-append out "/share"))
-                    )
-               ;(substitute*
-                   ;"src/CMakeLists.txt"
-                 ;(("COMMAND /bin/sh")
-                  ;(string-append "COMMAND " (which "sh"))))
-
-               ;(substitute*
-                   ;"libraries/zmusic/mididevices/music_fluidsynth_mididevice.cpp"
-                 ;(("/usr/share/sounds/sf2/FluidR3_GM.sf2")
-                  ;(string-append fluid-3 "/share/soundfonts/FluidR3Mono_GM.sf3")))
-;
-               ;(substitute*
-                   ;"libraries/zmusic/mididevices/music_timiditypp_mididevice.cpp"
-                 ;(("exename = \"timidity\"")
-                  ;(string-append "exename = \"" timidity++ "/bin/timidity\"")))
-               #t))))
-               ))
+        ;; Remove some bundled libraries.  XXX There are more, but removing
+        ;; them would require, at least, patching the build system.
+        #~(with-directory-excursion "thirdparty"
+            ;(delete-file-recursively "game-music-emu")
+            (delete-file-recursively "zlib")))))
     (build-system cmake-build-system)
+    (arguments
+     (list #:tests? #f
+           #:configure-flags
+           #~(list (string-append
+                    "-DCMAKE_CXX_FLAGS:="
+                    "-DSHARE_DIR=\\\"" #$output "/share/\\\" "
+                    "-DGUIX_OUT_PK3=\\\"" #$output "/share/games/doom\\\"")
 
+                   ;; The build requires some extra convincing not to use the bundled
+                   ;; libgme previously deleted in the soure snippet.
+                   "-DFORCE_INTERNAL_GME=OFF"
+
+                   ;; Link libraries at build time instead of loading them at run time.
+                   "-DDYN_OPENAL=OFF"
+                   "-DDYN_FLUIDSYNTH=OFF"
+                   "-DDYN_MPG123=OFF"
+                   "-DDYN_SNDFILE=OFF")
+           #:phases
+           #~(modify-phases %standard-phases
+               (add-before 'configure 'fix-referenced-paths
+                 (lambda* (#:key inputs outputs #:allow-other-keys)
+                   (substitute* "CMakeLists.txt"
+                     (("/bin/sh")
+                      (search-input-file inputs "bin/sh")))
+
+                   (substitute* (string-append "source/mididevices/"
+                                               "music_fluidsynth_mididevice.cpp")
+                     (("/usr/share/sounds/sf2/FluidR3_GM.sf2")
+                      (search-input-file
+                       inputs "share/soundfonts/FluidR3Mono_GM.sf3"))))))))
     ;;TODO ADL and OPl are not present
     ;;I think it'll use the ones in thirdparty/ idk
-    (inputs `(
-              ;("bzip2" ,bzip2) ;seems to be unused
-              ("fluid-3" ,fluid-3)
-              ("fluidsynth" ,fluidsynth)
-              ("libgme" ,libgme)
-              ("libsndfile" ,libsndfile)
-              ("mpg123" ,mpg123)
-              ("openal" ,openal)
-              ("wildmidi" ,wildmidi)
-              ("timidity++" ,timidity++)
-              ("libtimidity" ,libtimidity)
-              ("zlib" ,zlib)))
+    (inputs
+     (list fluid-3 fluidsynth libgme libsndfile mpg123
+           openal wildmidi timidity++ libtimidity zlib))
     (native-inputs (list pkg-config unzip))
     (synopsis "Modern Doom 2 source port")
     (description "GZdoom is a port of the Doom 2 game engine, with a modern
@@ -109,13 +81,11 @@ and the new ZScript language.  In addition to Doom, it supports Heretic, Hexen,
 Strife, Chex Quest, and fan-created games like Harmony, Hacx and Freedoom.")
     (home-page "https://zdoom.org/index")
     ;; The source uses x86 assembly
-    (supported-systems '("x86_64-linux" "i686-linux"))
+    (supported-systems (list "x86_64-linux" "i686-linux"))
     (license (list license:gpl3+         ; gzdoom game
                    license:lgpl3+        ; gzdoom renderer
                    license:expat         ; gdtoa
                    (license:non-copyleft ; modified dumb
                     "file://dumb/licence.txt"
-                    "Dumb license, explicitly GPL compatible.")))
-    ))
-
+                    "Dumb license, explicitly GPL compatible.")))))
 zmusic
